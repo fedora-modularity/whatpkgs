aad008cc84a5698e129f4ae092f245b31a6cf639
Stephen Gallagher <sgallagh@redhat.com>
2016-11-02 13:45:27 -0400

    Move glibc and gcc to BR
    Move fedora-logos and freetype to shared components.

--- split-base-and-system.sh
+++ split-base-and-system.sh
@@ -1,14 +1,46 @@
 #!/usr/bin/bash
 
+
+# Packages that explicitly belong in the Base Runtime
+EXPLICIT_BR="
+    dracut
+    gcc
+    gcc-c++
+    glibc
+    glibc-common
+    glibc-headers
+    grub2
+    grub2-efi
+    kernel-core
+    kernel-headers
+    libcrypt
+    libgcc
+    libstdc++
+    libstdc++-devel
+"
+
+# Packages that explicitly belong in the System Runtime
+EXPLICIT_SR="
+"
+
+# Packages that belong in the shared components
+EXPLICIT_SHARED="
+    fedora-logos \
+    freetype
+"
+
+
+
 echo
 echo "Generate the SRPM list for base runtime ("lights-on")"
 ./whatpkgs.py neededby --merge --no-recommends \
                       --hint=glibc-minimal-langpack \
                       --hint=fedora-release \
-                      --hint=libcrypt-nss \
+                      --hint=libcrypt \
                       --hint=cronie-noanacron \
                       --hint=coreutils \
-                      grub2 grub2-efi kernel-core kernel-headers \
+                      ${EXPLICIT_BR} \
+                      ${EXPLICIT_SHARED} \
 | xargs \
 ./whatpkgs.py getsourcerpm --full-name \
 | sort | tee sampledata/fedora/25beta/base-runtime-module-source-packages-prelim.txt
@@ -20,13 +52,25 @@ cat sampledata/toplevel-binary-packages.txt|xargs \
 ./whatpkgs.py neededby --merge --no-recommends \
                        --hint=glibc-minimal-langpack \
                        --hint=fedora-release \
-                       --hint=libcrypt-nss \
+                       --hint=libcrypt \
                        --hint=cronie-noanacron \
                        --hint=coreutils \
+                       --filter dracut \
+                       --filter gcc \
+                       --filter gcc-c++ \
+                       --filter glibc \
+                       --filter glibc-common \
+                       --filter glibc-headers \
                        --filter grub2 \
                        --filter grub2-efi \
                        --filter kernel-core \
                        --filter kernel-headers \
+                       --filter libcrypt \
+                       --filter libgcc \
+                       --filter libstdc++ \
+                       --filter libstdc++-devel \
+                       ${EXPLICIT_SR} \
+                       ${EXPLICIT_SHARED} \
 | xargs \
 ./whatpkgs.py getsourcerpm --full-name \
 | sort | tee sampledata/fedora/25beta/system-runtime-module-source-packages-prelim.txt
