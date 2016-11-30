c7a48860b044efde8ecae4aaff52dc8499c416df
Stephen Gallagher <sgallagh@redhat.com>
2016-11-30 08:25:16 -0500

    Sort hint lists
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- sampledata/fedora/25/update_sampledata.sh
+++ sampledata/fedora/25/update_sampledata.sh
@@ -9,22 +9,22 @@ cat sampledata/fedora/25/toplevel-binary-packages.txt| xargs \
 
 cat sampledata/fedora/25/toplevel-binary-packages.txt|xargs \
 ./whatpkgs.py neededby --merge --no-recommends \
-                       --hint=glibc-minimal-langpack \
-                       --hint=fedora-release \
-                       --hint=libcrypt-nss \
-                       --hint=cronie-noanacron \
                        --hint=coreutils \
+                       --hint=cronie-noanacron \
+                       --hint=fedora-release \
+                       --hint=glibc-minimal-langpack \
+                       --hint=libcrypt-nss \
                        --hint=python-libs \
 | tee sampledata/fedora/25/runtime-binary-dependency-packages-short.txt
 
 
 cat sampledata/fedora/25/toplevel-binary-packages.txt|xargs \
 ./whatpkgs.py neededby --merge --no-recommends --full-name \
-                       --hint=glibc-minimal-langpack \
-                       --hint=fedora-release \
-                       --hint=libcrypt-nss \
-                       --hint=cronie-noanacron \
                        --hint=coreutils \
+                       --hint=cronie-noanacron \
+                       --hint=fedora-release \
+                       --hint=glibc-minimal-langpack \
+                       --hint=libcrypt-nss \
                        --hint=python-libs \
 | tee sampledata/fedora/25/runtime-binary-dependency-packages-full.txt
 
@@ -42,64 +42,64 @@ cat sampledata/fedora/25/runtime-binary-dependency-packages-short.txt| xargs \
 cat sampledata/fedora/25/toplevel-binary-packages.txt | xargs \
 ./whatpkgs.py neededtoselfhost --merge --no-recommends --full-name \
                                --no-sources \
-                               --hint=glibc-minimal-langpack \
-                               --hint=fedora-release \
-                               --hint=libcrypt-nss \
-                               --hint=cronie-noanacron \
                                --hint=coreutils \
-                               --hint=java-1.8.0-openjdk-javadoc \
-                               --hint=sendmail \
+                               --hint=cronie-noanacron \
                                --hint=environment-modules \
                                --hint=fedora-logos-httpd \
-                               --hint=rubygem-minitest \
-                               --hint=rubygem-rspec \
+                               --hint=fedora-release \
+                               --hint=glibc-minimal-langpack \
+                               --hint=gnuplot \
+                               --hint=infinipath-psm \
+                               --hint=java-1.8.0-openjdk-javadoc \
                                --hint=kernel-core \
                                --hint=kernel-devel \
-                               --hint=gnuplot \
-                               --hint=perl-Archive-Extract-lzma-Compress-unLZMA \
-                               --hint=perl-Archive-Extract-tgz-tar-gzip \
-                               --hint=perl-Archive-Extract-Z-uncompress \
+                               --hint=libcrypt-nss \
+                               --hint=libverto-libev \
                                --hint=perl-Archive-Extract-bz2-bunzip2 \
                                --hint=perl-Archive-Extract-gz-gzip \
+                               --hint=perl-Archive-Extract-lzma-Compress-unLZMA \
                                --hint=perl-Archive-Extract-tar-tar \
-                               --hint=perl-Archive-Extract-txz-tar-unxz \
-                               --hint=perl-Archive-Extract-zip-unzip \
                                --hint=perl-Archive-Extract-tbz-tar-bunzip2 \
+                               --hint=perl-Archive-Extract-tgz-tar-gzip \
+                               --hint=perl-Archive-Extract-txz-tar-unxz \
                                --hint=perl-Archive-Extract-xz-unxz \
-                               --hint=libverto-libev \
-                               --hint=infinipath-psm \
+                               --hint=perl-Archive-Extract-zip-unzip \
+                               --hint=perl-Archive-Extract-Z-uncompress \
+                               --hint=rubygem-minitest \
+                               --hint=rubygem-rspec \
+                               --hint=sendmail \
 | tee sampledata/fedora/25/selfhosting-binary-packages-full.txt
 
 
 cat sampledata/fedora/25/toplevel-binary-packages.txt | xargs \
 ./whatpkgs.py neededtoselfhost --merge --no-recommends \
                                --no-sources \
-                               --hint=glibc-minimal-langpack \
-                               --hint=fedora-release \
-                               --hint=libcrypt-nss \
-                               --hint=cronie-noanacron \
                                --hint=coreutils \
-                               --hint=java-1.8.0-openjdk-javadoc \
-                               --hint=sendmail \
+                               --hint=cronie-noanacron \
                                --hint=environment-modules \
                                --hint=fedora-logos-httpd \
-                               --hint=rubygem-minitest \
-                               --hint=rubygem-rspec \
+                               --hint=fedora-release \
+                               --hint=glibc-minimal-langpack \
+                               --hint=gnuplot \
+                               --hint=infinipath-psm \
+                               --hint=java-1.8.0-openjdk-javadoc \
                                --hint=kernel-core \
                                --hint=kernel-devel \
-                               --hint=gnuplot \
-                               --hint=perl-Archive-Extract-lzma-Compress-unLZMA \
-                               --hint=perl-Archive-Extract-tgz-tar-gzip \
-                               --hint=perl-Archive-Extract-Z-uncompress \
+                               --hint=libcrypt-nss \
+                               --hint=libverto-libev \
                                --hint=perl-Archive-Extract-bz2-bunzip2 \
                                --hint=perl-Archive-Extract-gz-gzip \
+                               --hint=perl-Archive-Extract-lzma-Compress-unLZMA \
                                --hint=perl-Archive-Extract-tar-tar \
-                               --hint=perl-Archive-Extract-txz-tar-unxz \
-                               --hint=perl-Archive-Extract-zip-unzip \
                                --hint=perl-Archive-Extract-tbz-tar-bunzip2 \
+                               --hint=perl-Archive-Extract-tgz-tar-gzip \
+                               --hint=perl-Archive-Extract-txz-tar-unxz \
                                --hint=perl-Archive-Extract-xz-unxz \
-                               --hint=libverto-libev \
-                               --hint=infinipath-psm \
+                               --hint=perl-Archive-Extract-zip-unzip \
+                               --hint=perl-Archive-Extract-Z-uncompress \
+                               --hint=rubygem-minitest \
+                               --hint=rubygem-rspec \
+                               --hint=sendmail \
 | tee sampledata/fedora/25/selfhosting-binary-packages-short.txt
 
 cat sampledata/fedora/25/selfhosting-binary-packages-short.txt | xargs \
