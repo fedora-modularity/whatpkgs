4147bf3a28428068d757703a6bfc3364a430f98c
Petr Šabata <contyk@redhat.com>
2017-02-17 16:02:31 +0100

    Add a simple script to get dist-git hashes for a list of source ENVRAs
    Signed-off-by: Petr Šabata <contyk@redhat.com>

---
+++ get-dist-git-hashes.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use MCE::Loop;
+
+my $if = shift @ARGV or die "Usage: get-dist-git-hashes.pl <selfhosting-source-packages-full.txt>\n";
+
+my @pkgs;
+open my $fh, '<', $if or die "Cannot open '${if}': $!\n";
+while (<$fh>) {
+    chomp;
+    /^\d+:(.+)\.src$/;
+    push @pkgs, $1;
+}
+close $fh or die "Cannot close '${if}': $!\n";
+
+MCE::Loop::init { max_workers => 8 };
+
+mce_loop {
+    my ($mce, $cref, $cid) = @_;
+    for (@$cref) {
+        my $output = `koji buildinfo $_`;
+        $output =~ /\/rpms\/[^:]+:([a-f0-9]+)/;
+        MCE->say($_ . ": $1");
+    }
+} @pkgs;
